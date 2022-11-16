81b15d5dab250a89b83a236f81ed2c75017202e2
Sheikh Suhail Khursheed <70643928+SheikhSuhail19@users.noreply.github.com>
2022-11-16 16:02:57 +0530

    Time: 17 ms (98.57%), Space: 13 MB (97.46%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,12 +1,9 @@
-class Solution
-{
+class Solution{
     public:
 
-    vector<int> findOrder(int numCourses, vector<vector < int>> &prerequisites)
-    {
+    vector<int> findOrder(int numCourses, vector<vector < int>> &prerequisites){
         vector<int> adj[numCourses];
-        for (int i = 0; i < prerequisites.size(); i++)
-        {
+        for (int i = 0; i < prerequisites.size(); i++){
             int u = prerequisites[i][0];
             int v = prerequisites[i][1];
             adj[v].push_back(u);
@@ -14,34 +11,27 @@ class Solution
         int V = numCourses;
         int indegree[V];
         memset(indegree,0,sizeof indegree);
-        for (int i = 0; i < V; i++)
-        {
-            for (auto it: adj[i])
-            {
+        for (int i = 0; i < V; i++){
+            for (auto it: adj[i]){
                 indegree[it]++;
             }
         }
 
         queue<int> q;
-        for (int i = 0; i < V; i++)
-        {
-            if (indegree[i] == 0)
-            {
+        for (int i = 0; i < V; i++){
+            if (indegree[i] == 0){
                 q.push(i);
             }
         }
 
         vector<int> schedule;
-        while (!q.empty())
-        {
+        while (!q.empty()){
             int node = q.front();
             q.pop();
             schedule.push_back(node);
-            for (auto it: adj[node])
-            {
+            for (auto it: adj[node]){
                 indegree[it]--;
-                if (indegree[it] == 0)
-                {
+                if (indegree[it] == 0){
                     q.push(it);
                 }
             }
