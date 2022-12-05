dcd711c1e5b9679a0e66d7a83c1792f7a5f1006c
Sheikh Suhail Khursheed <70643928+SheikhSuhail19@users.noreply.github.com>
2022-12-05 17:17:57 +0530

    Added solution - LeetHub

--- Kth smallest element - GFG/kth-smallest-element.cpp
+++ Kth smallest element - GFG/kth-smallest-element.cpp
@@ -7,26 +7,66 @@ using namespace std;
 // } Driver Code Ends
 //User function template for C++
 
+// class Solution{
+//     public:
+//     // arr : given array
+//     // l : starting index of the array i.e 0
+//     // r : ending index of the array i.e size-1
+//     // k : find kth smallest element and return using this function
+//     int kthSmallest(int arr[], int l, int r, int k) {
+//         priority_queue<int> pq;
+//         for(int i = 0; i < k; i++) {
+//             pq.push(arr[i]);
+//         }
+        
+//         for(int i = k; i <= r; i++) {
+//             if(arr[i] < pq.top()) {
+//                 pq.pop();
+//                 pq.push(arr[i]);
+//             }
+//         }
+        
+//         return pq.top();
+//     }
+// };
+
 class Solution{
     public:
+    
+    int partition(int* &arr, int l, int r) {
+        int pivot = arr[r];
+        int i = l-1;
+        for(int j = l; j < r; j++) {
+            if(arr[j] <= pivot) {
+                swap(arr[++i],arr[j]);
+            }
+        }
+        swap(arr[++i],arr[r]);
+        return i;
+    }
+    
+    int quick(int* &arr, int l, int r, int k) {
+        if(k > 0 && k <= r - l + 1) {
+            
+            int pivot = partition(arr,l,r);
+            
+            if(pivot-l == k-1)
+                return arr[pivot];
+                
+            if(pivot-l > k-1)
+                return quick(arr,l,pivot-1,k);
+                
+            return quick(arr,pivot+1,r,k-pivot+l-1);
+        }
+        return INT_MAX;
+    }
+    
     // arr : given array
     // l : starting index of the array i.e 0
     // r : ending index of the array i.e size-1
     // k : find kth smallest element and return using this function
     int kthSmallest(int arr[], int l, int r, int k) {
-        priority_queue<int> pq;
-        for(int i = 0; i < k; i++) {
-            pq.push(arr[i]);
-        }
-        
-        for(int i = k; i <= r; i++) {
-            if(arr[i] < pq.top()) {
-                pq.pop();
-                pq.push(arr[i]);
-            }
-        }
-        
-        return pq.top();
+        return quick(arr,l,r,k);
     }
 };
 
